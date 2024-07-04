7a158bafb8c8b1987bfa087e6a9c07a63f853804
jokw1228 <jokw1228@naver.com>
2024-07-05 02:25:14 +0900

    존경하는 교수님

--- scenes/rooms/RoomGrade.gd
+++ scenes/rooms/RoomGrade.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 @export var GradeGenerator_node: GradeGenerator
+@export var jhan_node: Sprite2D
 
 func _ready():
 	GradeGenerator_node.generate_grade()
+	await get_tree().create_timer(9.0).timeout
+	jhan_node.visible = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(jhan_node, "modulate", Color(1, 1, 1, 0.2), 1.0)
