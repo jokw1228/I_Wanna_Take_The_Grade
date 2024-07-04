c7176d64adc6b2f14fcce7d857e668170e05c184
jokw1228 <jokw1228@naver.com>
2024-07-04 22:02:51 +0900

    각 텍스트들을 PackedScene으로 만듦

--- scenes/rooms/RoomGrade.gd
+++ scenes/rooms/RoomGrade.gd
@@ -1,54 +1,74 @@
 extends Node2D
 
-@export var BGM_node: AudioStreamPlayer2D
+@export var subject: String
+@export var list_to_print_left: Array[String]
+@export var list_to_print_right: Array[String]
+@export var grade: String
 
+@export var LabelSubject_scene: PackedScene
+@export var LabelLeft_scene: PackedScene
+@export var LabelRight_scene: PackedScene
+@export var LabelGrade_scene: PackedScene
+
+@export var BGM_node: AudioStreamPlayer2D
 @export var SFX_Subject: AudioStreamPlayer2D
 @export var SFX_Labeling: AudioStreamPlayer2D
 @export var SFX_Graded: AudioStreamPlayer2D
 
-@export var Label_Subject: Label
-
-@export var Midterm: Node
-@export var Assignment: Node
-@export var Final: Node
-
-@export var Label_Rank: Label
-@export var Label_Grade: Label
+var list_size: int
 
 func _ready():
+	list_size = list_to_print_left.size()
 	
 	await get_tree().create_timer(0.5).timeout
 	
-	Label_Subject.visible = true
+	# subject
+	var inst_subject = LabelSubject_scene.instantiate()
+	inst_subject.text = subject.replace("\\n", "\n")
+	inst_subject.position = Vector2(32, 64)
+	add_child(inst_subject)
 	SFX_Subject.play()
 	
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3).timeout # wait to the end of SFX_subject
 	
+	# stage clear BGM
 	BGM_node.play()
 	
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(1.0).timeout # wait a moment before showing the score for each item
 	
-	Midterm.visible = true
-	SFX_Labeling.play()
+	### scores ###
 	
+	for i in range(list_size):
+		var inst_left = LabelLeft_scene.instantiate()
+		inst_left.text = list_to_print_left[i]
+		inst_left.position = Vector2(0, 160 + 48 * i)
+		add_child(inst_left)
+		var inst_right = LabelRight_scene.instantiate()
+		inst_right.text = list_to_print_right[i]
+		inst_right.position = Vector2(920, 160 + 48 * i)
+		add_child(inst_right)
+		
+		SFX_Labeling.play()
+		await get_tree().create_timer(0.5).timeout
+	
+	###############
+	
+	# rank
 	await get_tree().create_timer(0.5).timeout
 	
-	Assignment.visible = true
+	var inst_rank = LabelLeft_scene.instantiate()
+	inst_rank.text = "rank"
+	inst_rank.position = Vector2(0, 432)
+	add_child(inst_rank)
 	SFX_Labeling.play()
 	
-	await get_tree().create_timer(0.5).timeout
-	
-	Final.visible = true
-	SFX_Labeling.play()
-	
+	# grade
 	await get_tree().create_timer(1.0).timeout
 	
-	Label_Rank.visible = true
-	SFX_Labeling.play()
-	
-	await get_tree().create_timer(1.0).timeout
-	
-	Label_Grade.visible = true
+	var inst_grade = LabelGrade_scene.instantiate()
+	inst_grade.text = grade
+	inst_grade.position = Vector2(928, 376)
+	add_child(inst_grade)
 	SFX_Graded.play()
 
 func _process(_delta):
