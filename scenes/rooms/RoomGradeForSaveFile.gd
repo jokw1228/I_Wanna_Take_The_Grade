d38c6cbd3672aa86bf3ab4acda4fa233c0171f50
jokw1228 <jokw1228@naver.com>
2024-07-05 01:32:13 +0900

    misc

--- scenes/rooms/RoomGradeForSaveFile.gd
+++ scenes/rooms/RoomGradeForSaveFile.gd
@@ -25,13 +25,27 @@ func _ready():
 func load_savefile():
 	var load_file = FileAccess.open(SAVE_PATH, FileAccess.READ)
 
-	if FileAccess.file_exists(SAVE_PATH) == true:
-		if not load_file.eof_reached():
-			var current_line = JSON.parse_string(load_file.get_line())
-			if current_line:
-				GradeGenerator_node.subject = current_line["subject"]
-				GradeGenerator_node.subject_color = current_line["subject_color"]
-				GradeGenerator_node.list_to_print_left = current_line["list_to_print_left"]
-				GradeGenerator_node.list_to_print_right = current_line["list_to_print_right"]
-				GradeGenerator_node.grade = current_line["grade"]
-				GradeGenerator_node.grade_color = current_line["grade_color"]
+	if FileAccess.file_exists(SAVE_PATH) == false:
+		return
+	
+	var current_line = JSON.parse_string(load_file.get_line())
+	if current_line:
+		#subject
+		GradeGenerator_node.subject = current_line["subject"]
+		
+		#subject_color
+		var subject_color_parse = current_line["subject_color"]
+		var subject_color_splitted = subject_color_parse.replace("(", "").replace(")", "").split(",")
+		GradeGenerator_node.subject_color = Color(float(subject_color_splitted[0]), float(subject_color_splitted[1]), float(subject_color_splitted[2]), float(subject_color_splitted[3]))
+		
+		print(current_line["list_to_print_left"])
+		#GradeGenerator_node.list_to_print_left = current_line["list_to_print_left"]
+		#GradeGenerator_node.list_to_print_right = current_line["list_to_print_right"]
+		
+		#grade
+		GradeGenerator_node.grade = current_line["grade"]
+		
+		#grade_color
+		var grade_color_parse = current_line["grade_color"]
+		var grade_color_splitted = grade_color_parse.replace("(", "").replace(")", "").split(",")
+		GradeGenerator_node.grade_color = Color(float(grade_color_splitted[0]), float(grade_color_splitted[1]), float(grade_color_splitted[2]), float(grade_color_splitted[3]))
